f4e59340d78b54fe781376e8be19ee3880e3c2bb
atomicng365 <36950054+atomicng365@users.noreply.github.com>
2025-10-13 22:52:46 +0000

    hi

--- 5.A.1.py
+++ 5.A.1.py
@@ -1,8 +1,27 @@
 f = open('Marks.txt','r')
 a = f.readlines()
 f.close()
-an = []
+e = open('Groups.txt','r')
+b = e.readlines()
+e.close()
+marks = []
+groups = []
+names = []
+g = open('Names.txt','r')
+c = g.readlines()
+g.close()
 # convert into int
 for item in a:
-    an = an + [int(item)]
-print(an)
+    marks = marks + [int(item)]
+
+for item in c:
+    names = names + [item.rstrip()]
+
+for item in b:
+    groups = groups + [item.rstrip()]
+
+
+excel = []
+for i in range(0,len(marks)):
+    excel = excel + [(names[i],groups[i],marks[i])]
+print(excel)
